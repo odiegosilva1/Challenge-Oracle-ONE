36124e5cd879cda96d98602eb6c924a56433a0a8
odiegosilva1 <odiegosilvadev@gmail.com>
2022-12-23 21:15:17 -0300

    add css

--- js/script.js
+++ js/script.js
@@ -1,10 +1,61 @@
-/**
- * e = "enter"
- * i = "imes"
- * a = "ai"
- * o = "ober"
- * u = "ufat"
- */
 
-var a = "ai"
-var e =  "enter"
+function encriptar(){
+    var str = document.getElementById('str').value;
+    limpaInput();
+
+    var inputCripot = str.normalize("NFD").replace(/[^a-zA-Z\s]/g, "").toLowerCase();
+    
+    var mensEncript = inputCripot.replace(/e/g, 'enter');
+    mensEncript = mensEncript.replace(/i/g, 'imes');
+    mensEncript = mensEncript.replace(/a/g, 'ai');
+    mensEncript = mensEncript.replace(/o/g, 'ober');
+    mensEncript = mensEncript.replace(/u/g, 'ufat');
+    
+    document.getElementById('resultado').value = mensEncript;
+    
+    }
+    
+    
+    
+    function limpaInput(){
+        document.getElementById('str').value='';
+    }
+    
+    /**
+     * Pegamos o texo ques esta encriptado e percorreremos o caminha contrario
+     */
+    function desencriptar(){
+    
+        var str = document.getElementById('str').value;
+    
+        var mensDesenEncript = str.replace(/enter/g, 'e');
+        mensDesenEncript = mensDesenEncript.replace(/imes/g, 'i');
+        mensDesenEncript = mensDesenEncript.replace(/ai/g, 'a');
+        mensDesenEncript = mensDesenEncript.replace(/ober/g, 'o');
+        mensDesenEncript = mensDesenEncript.replace(/ufat/g, 'u');
+    
+    
+        document.getElementById('resultado').value = mensDesenEncript;
+        limpaInput();
+    
+    }
+    /**
+     * Função para copiar o texto 
+     * Rererencia
+     * https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+     */
+    function copiarMensagem() {
+        
+        var copyText = document.getElementById("resultado");
+        
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); // For mobile devices
+      
+       
+        navigator.clipboard.writeText(copyText.value);
+        document.getElementById("resultado").value = 'Texto Copiado!';
+       
+      }
+      
+    
+     
